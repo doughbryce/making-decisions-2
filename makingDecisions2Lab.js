f394a1870c7b6a081282c3a83c3dd5f598b666a9
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-04-14 14:10:16 -0700

    problem 9 functional

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -129,9 +129,11 @@ let listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 for (let i = 0; i < listOfNumbers.length; i++) {
+
   if (listOfNumbers[i] % 3 === 0) {
     console.log(`${listOfNumbers[i]} is divisible by 3`)
   }
+
 }
 
 
@@ -144,7 +146,9 @@ let letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
 */
 
-//Code Here
+for (let i = letters.length - 1; i >= 0; i--) {
+  console.log(letters[i]);
+}
 
 
 ////////// Advanced Problems //////////
