dd0d6d568984942b4dd0381ce757b32c5ce86299
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-04-14 13:18:29 -0700

    problem 5 functional

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -61,14 +61,26 @@ console.log(evensArr);
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-let score = 74
+let score = 74;
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+if (score <= 100 && score >= 90) {
+  console.log(`A`);
+} else if (score < 90 && score >= 80){
+  console.log(`B`);
+} else if (score < 80 && score >= 70) {
+  console.log(`C`);
+} else if (score < 70 && score >= 60) {
+  console.log(`D`);
+} else if (score < 60 && score >= 0) {
+  console.log(`F`);
+} else {
+  console.log(`invalid score`);
+}
 
 
 ////////// Intermediate Problems //////////
